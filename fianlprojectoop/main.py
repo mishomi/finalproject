fb2028671d3dfe1273176e8f617420a088468029
KIU-PC\Kiu-Student <bakhbakhashvili.mikheil@kiu.edu.ge>
2024-03-08 10:47:31 +0400

    added bookdata json file, changed my code to save all the data in it and added a checker to see if added book is already in bookdata.json

--- fianlprojectoop/main.py
+++ fianlprojectoop/main.py
@@ -1,3 +1,5 @@
+import json
+
 class Book:
     def __init__(self, title, author, release_year):
         self.title = title
@@ -7,18 +9,35 @@ class Book:
 class BookManager:
     def __init__(self):
         self.books = []
+        self.load_books()
+
+    def load_books(self):
+        try:
+            with open('bookdata.json', 'r') as f:
+                self.books = json.load(f)
+        except FileNotFoundError:
+            self.books = []
+
+    def save_books(self):
+        with open('bookdata.json', 'w') as f:
+            json.dump(self.books, f, indent=4)
 
     def add_book(self, book):
-        self.books.append(book)
+        for existing_book in self.books:
+            if (existing_book['title'] == book.title and existing_book['author'] == book.author and existing_book['release_year'] == book.release_year):
+                return False
+        self.books.append({'title': book.title, 'author': book.author, 'release_year': book.release_year})
+        self.save_books()
+        return True
 
     def display_all_books(self):
         for book in self.books:
-            print('title: ', book.title, '\nauthor: ', book.author, '\nrelease year: ', book.release_year)
+            print('\ntitle: ', book['title'], '\nauthor: ', book['author'], '\nrelease year: ', book['release_year'])
 
     def search_book(self, title):
         found_books = []
         for book in self.books:
-            if title.lower() in book.title.lower():  # case-insensitive partial match
+            if title.lower() in book['title'].lower():
                 found_books.append(book)
         return found_books
 
@@ -42,28 +61,26 @@ def main():
                 else:
                     print("Please enter a valid year (as a number).")
             new_book = Book(title, author, year)
-            book_manager.add_book(new_book)
-            print("Book added successfully!")
-
+            if book_manager.add_book(new_book):
+                print("Book added successfully!")
+            else:
+                print("Book already in the database")
         elif choice == "2":
             print("\n--- All Books ---")
             book_manager.display_all_books()
             print("-----------------")
-
         elif choice == "3":
             title = input("Enter the title of the book you want to search for: ")
             found_books = book_manager.search_book(title)
             if found_books:
                 print("Books found:")
                 for book in found_books:
-                    print('title: ', book.title, '\nauthor: ', book.author, '\nrelease year: ', book.release_year)
+                    print('title: ', book['title'], '\nauthor: ', book['author'], '\nrelease year: ', book['release_year'])
             else:
                 print("Book not found.")
-
         elif choice == "4":
             print("Exiting...")
             break
-
         else:
             print("Invalid choice. Please enter a number from 1 to 4.")
 
